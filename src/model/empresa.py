59a3abaa5c0a3d15625beb765cc15d9abc893679
davipadua <davipadua1@gmail.com>
2022-09-15 09:44:58 -0300

    model empresa atualizada

--- src/model/empresa.py
+++ src/model/empresa.py
@@ -7,10 +7,10 @@ class EmpresaHelper:
         return {
             'nomeUsuario': item.get('nome_usuario'),
             'situacaoConta': item.get('situacao_conta'),
-            'lingua': item.get('lingua'),
             'tipoArmazenagem': item.get('tipo_armazenagem'),
             'aceiteTermosUso': item.get('aceite_termos_uso'),
-            'usuarioId': item.get('usuario_fk')
+            'usuarioId': item.get('usuario_fk'),
+            'razaoSocial': item.get('razao_social')
         }
 
 
@@ -21,22 +21,22 @@ def query_all(usuario_id: str, nome: str = None):
     return database.select_all(query=query)
 
 
-# def query_one(empresa_id: str, almoxarifado_id: int):
-#     query = f"SELECT * from almoxarifado WHERE empresa_fk = {empresa_id} AND id = {almoxarifado_id}"
-#     return database.select_one(query=query)
+def query_one(usuario_fk: str):
+    query = f"SELECT * from empresa WHERE usuario_fk = {usuario_fk}"
+    return database.select_one(query=query)
 
 
 def execute_create(item: dict):
     query = f" INSERT INTO empresa (nome_usuario, situacao_conta, lingua, tipo_armazenagem," \
             f" aceite_termos_uso, usuario_fk)VALUES " \
-            f" ('{item['nomeUsuario']}', '{item['situacaoConta']}', '{item['lingua']}', '{item['tipoArmazenagem']}', " \
-            f" ('{item['aceiteTermosUso']}', '{item['usuarioId']}'); "
+            f" ('{item['nomeUsuario']}', '{item['situacaoConta']}', '{item['tipoArmazenagem']}', " \
+            f" ('{item['aceiteTermosUso']}', '{item['usuarioId']}', '{item['razaoSocial']}'); "
     database.execute(query=query)
 
 
 def execute_update(item: dict):
-    query = f" UPDATE empresa SET nome_usuario = '{item['nomeUsuario']}', SET situacao_conta = '{item['situacaoConta']}', " \
-            f" SET lingua = '{item['lingua']}', SET tipo_armazenagem = '{item['tipoArmazenagem']}' " \
+    query = f" UPDATE empresa SET nome_usuario = '{item['nomeUsuario']}', situacao_conta = '{item['situacaoConta']}', " \
+            f" tipo_armazenagem = '{item['tipoArmazenagem']}' " \
             f" WHERE usuario_fk = '{item['usuarioID']}' "
     database.execute(query=query)
 
