a28825f225f45c7802cb8cb61dfd10126ef95f36
davipadua <davipadua1@gmail.com>
2022-09-16 15:52:28 -0300

    model operador atualizado

--- src/model/operador.py
+++ src/model/operador.py
@@ -12,31 +12,28 @@ class FuncionarioHelper:
         }
 
 
-def query_all(usuario_id: str, nome: str = None):
-    query = f"SELECT * from funcionario WHERE usuario_fk = {usuario_id}"
-    if nome:
-        query += f" AND nome_usuario = '{nome}'"
+def query_all(empresa_id: str):
+    query = f"SELECT * from operador WHERE empresa_fk = {empresa_id}"
     return database.select_all(query=query)
 
 
 def query_one(empresa_id: str, usuario_id: int):
-    query = f"SELECT * from funcionario WHERE empresa_fk = {empresa_id} AND id = {usuario_id}"
+    query = f"SELECT * from operador WHERE empresa_fk = {empresa_id} AND usuario_fk = {usuario_id}"
     return database.select_one(query=query)
 
 
 def execute_create(item: dict):
-    query = f" INSERT INTO funcionario (nome_usuario, acesso, empresa_fk, usuario_fk," \
-            f" aceite_termos_uso, usuario_fk)VALUES " \
+    query = f" INSERT INTO operador (nome_usuario, acesso, empresa_fk, usuario_fk)VALUES " \
             f" ('{item['nomeUsuario']}', '{item['acesso']}', '{item['empresaId']}', '{item['usuarioId']}'"
     database.execute(query=query)
 
 
 def execute_update(item: dict):
-    query = f" UPDATE funcionario SET nome_usuario = '{item['nomeUsuario']}', SET acesso = '{item['acesso']}', " \
-            f" WHERE usuario_fk = '{item['usuarioID']}' "
+    query = f" UPDATE operador SET nome_usuario = '{item['nomeUsuario']}', acesso = '{item['acesso']}', " \
+            f" WHERE usuario_fk = '{item['usuarioID']}' AND empresa_fk = {item['empresaId']} "
     database.execute(query=query)
 
 
-def execute_delete(usuario_fk: str):
-    query = f"DELETE FROM funcionario WHERE usuario_fk = '{usuario_fk}'"
+def execute_delete(usuario_fk: str, empresa_fk: str):
+    query = f"DELETE FROM operador WHERE usuario_fk = '{usuario_fk}' AND empresa_fk = {empresa_fk}"
     database.execute(query=query)
