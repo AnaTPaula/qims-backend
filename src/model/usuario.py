6353b1b2523a11ef88d20f8e81090bb3522d528a
davipadua <davipadua1@gmail.com>
2022-09-16 16:09:26 -0300

    model usuario atualizado

--- src/model/usuario.py
+++ src/model/usuario.py
@@ -12,29 +12,28 @@ class UsuarioHelper:
         }
 
 
-
-def query_all(usuario_id: str):
-    query = f"SELECT * from usuario WHERE id = {usuario_id}"
+def query_all():
+    query = f" SELECT * from usuario "
     return database.select_all(query=query)
 
 
-# def query_one(usuario_id: str, almoxarifado_id: int):
-#     query = f"SELECT * from almoxarifado WHERE empresa_fk = {empresa_id} AND id = {almoxarifado_id}"
-#     return database.select_one(query=query)
+def query_one(usuario_id: str):
+    query = f" SELECT * from usuario WHERE id = {usuario_id} "
+    return database.select_one(query=query)
 
 
 def execute_create(item: dict):
-    query = f"INSERT INTO usuario (tipo, senha, data_cadastro) VALUES " \
-            f"('{item['tipo']}', '{item['senha']}', '{item['dataCadastro']}');"
+    query = f" INSERT INTO usuario (tipo, senha, data_cadastro) VALUES " \
+            f" ('{item['tipo']}', '{item['senha']}', '{item['dataCadastro']}'); "
     database.execute(query=query)
 
 
 def execute_update(item: dict):
-    query = f"UPDATE usuario SET tipo = '{item['tipo']}', SET senha = '{item['senha']}'" \
+    query = f" UPDATE usuario SET tipo = '{item['tipo']}', senha = '{item['senha']}' " \
             f" WHERE id = '{item['id']}'"
     database.execute(query=query)
 
 
 def execute_delete(usuario_id):
-    query = f"DELETE FROM usuario WHERE id = '{usuario_id}'"
+    query = f" DELETE FROM usuario WHERE id = '{usuario_id}' "
     database.execute(query=query)
