1c73518675ce7d87bbc649c7425e5ae22d53576d
davipadua <davipadua1@gmail.com>
2022-10-03 09:38:18 -0300

    service lote

--- src/model/lote.py
+++ src/model/lote.py
@@ -14,26 +14,26 @@ class LoteHelper:
         }
 
 
-def query_all(empresa_id: int):
+def query_all_lote(empresa_id: int):
     query = f" select * from lote where empresa_fk = %s "
     params = (empresa_id,)
     return database.select_all(query=query, params=params)
 
 
-def query_one(empresa_id: int, produto_id: int):
+def query_one_lote(empresa_id: int, lote_id: int):
     query = f" SELECT * from lote WHERE empresa_fk = %s AND id = %s "
-    params = (empresa_id, produto_id,)
+    params = (empresa_id, lote_id,)
     return database.select_one(query=query, params=params)
 
 
-def execute_create(item: dict):
+def execute_create_lote(item: dict):
     query = f" INSERT INTO lote (codigo_lote, data_entrada, data_validade, quantidade, empresa_fk) VALUES " \
             f" (%s, %s, %s, %s, %s) "
     params = (item['codigoLote'], item['dataEntrada'], item['dataValidade'], item['quantidade'], item['EmpresaId'],)
     database.execute(query=query, params=params)
 
 
-def execute_update(item: dict):
+def execute_update_lote(item: dict):
     query = " UPDATE lote SET codigo_lote = %s, data_entrada = %s, data_validade = %s, quantidade = %s " \
             " WHERE empresa_fk = %s AND id = %s "
     params = (item['codigoLote'], item['dataEntrada'], item['dataValidade'], item['quantidade'], item['empresaId'],
@@ -41,8 +41,7 @@ def execute_update(item: dict):
     database.execute(query=query, params=params)
 
 
-def execute_delete(empresa_id: int, lote_id: int):
+def execute_delete_lote(empresa_id: int, lote_id: int):
     query = f" DELETE FROM lote WHERE id = %s AND empresa_fk = %s "
     params = (empresa_id, lote_id)
     database.execute(query=query, params=params)
-    
---
+++ src/service/lote.py
@@ -0,0 +1,87 @@
+import logging
+
+from controller.api_helper import ApiError
+from model.lote import LoteHelper, query_all_lote, execute_create_lote, execute_update_lote, query_one_lote,\
+    execute_delete_lote
+
+
+def find(empresa_id: int):
+    try:
+        items = query_all_lote(empresa_id=empresa_id)
+        return [LoteHelper.serialize(item) for item in items]
+    except Exception as ex:
+        logging.error(ex)
+        raise ApiError()
+
+
+def get_item(empresa_id: int, lote_id: int):
+    try:
+        item = query_one_lote(empresa_id=empresa_id, lote_id=lote_id)
+        return LoteHelper.serialize(item) if item else {}
+    except Exception as ex:
+        logging.error(ex)
+        raise ApiError()
+
+
+def create(body: dict):
+    if not is_unique(body=body):
+        raise ApiError(error_code=400, error_message='Nome já existe.')
+    item = {
+        'codigoLote': body['codigoLote'],
+        'dataEntrada': body['dataEntrada'],
+        'dataValidade': body['dataValidade'],
+        'quantidade': body['quantidade'],
+        'empresaId': body['empresaId'],
+    }
+    try:
+        execute_create_lote(item=item)
+        return {}
+    except Exception as ex:
+        logging.error(ex)
+        raise ApiError()
+
+
+def update(body: dict):
+    if not is_unique(body=body):
+        raise ApiError(error_code=400, error_message='Nome já existe.')
+    try:
+        item = query_one_lote(empresa_id=body['empresaId'], lote_id=body['id'])
+        if item:
+            item['codigoLote'] = body['codigoLote']
+            item['dataEntrada'] = body['dataEntrada']
+            item['dataValidade'] = body['dataValidade']
+            item['quantidade'] = body['quantidade']
+            item['empresaId'] = body['empresaId']
+            item['id'] = body['id']
+            execute_update_lote(item=item)
+            return {}
+        else:
+            raise ApiError(error_code=404, error_message='Lote não encontrado')
+    except ApiError as err:
+        raise err
+    except Exception as ex:
+        logging.error(ex)
+        raise ApiError()
+
+
+def remove(empresa_id: int, lote_id: int):
+    try:
+        item = query_one_lote(empresa_id=empresa_id, lote_id=lote_id)
+        if item:
+            execute_delete_lote(empresa_id=empresa_id, lote_id=lote_id)
+        return {}
+    except Exception as ex:
+        logging.error(ex)
+        raise ApiError()
+
+
+def is_unique(body: dict):
+    try:
+        items = query_all_lote(empresa_id=body['empresaId'])
+        if body.get('id'):
+            return False if items and int(body.get('id')) != items[0].get('id') else True
+        else:
+            return False if items else True
+    except Exception as ex:
+        logging.error(ex)
+        raise ApiError()
