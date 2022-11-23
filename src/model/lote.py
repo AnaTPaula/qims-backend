d31c3c8cd222f640b17fcdf4d94952ce8c61b79f
davipadua <davipadua1@gmail.com>
2022-11-23 08:46:42 -0300

    adicionar nome produto em lote

--- src/model/lote.py
+++ src/model/lote.py
@@ -12,15 +12,17 @@ class LoteHelper:
             'dataValidade': item.get('data_validade'),
             'quantidade': item.get('quantidade'),
             'empresaId': item.get('empresa_fk'),
+            'ProdutoNome': item.get('nome'),
             'associado': True if item.get('produto_estoque_fk') else False,
         }
 
 
 def query_all_lote_grouped(empresa_id: int, codigo_lote: str):
     query = "select distinct on (qty.codigo_lote) qty.codigo_lote, qty.quantidade, l.id, l.data_entrada, " \
-            "l.data_validade, l.empresa_fk, el.produto_estoque_fk from lote l join (select codigo_lote, " \
+            "l.data_validade, l.empresa_fk, el.produto_estoque_fk, p.nome from lote l join (select codigo_lote, " \
             "coalesce(sum(quantidade),0) as quantidade from lote where empresa_fk = %s group by codigo_lote) as qty " \
-            "on l.codigo_lote = qty.codigo_lote left join estoque_lote el on l.id = el.lote_fk "
+            "on l.codigo_lote = qty.codigo_lote left join estoque_lote el on l.id = el.lote_fk " \
+            "left join produto_estoque pe on pe.id = el.produto_estoque_fk left join produto p on p.id = pe.produto_fk"
     if codigo_lote:
         query += " WHERE l.codigo_lote = %s "
         params = (empresa_id, codigo_lote,)
@@ -31,7 +33,8 @@ def query_all_lote_grouped(empresa_id: int, codigo_lote: str):
 
 
 def query_all_lote(empresa_id: int, codigo_lote: str):
-    query = "select l.*, produto_estoque_fk from lote l left join estoque_lote el on l.id = el.lote_fk " \
+    query = "SELECT l.*, el.produto_estoque_fk, p.nome from lote l left join estoque_lote el on el.lote_fk = l.id " \
+            "left join produto_estoque pe on pe.id = el.produto_estoque_fk left join produto p on p.id = pe.produto_fk " \
             "where l.empresa_fk = %s "
     if codigo_lote:
         query += " and codigo_lote = %s "
